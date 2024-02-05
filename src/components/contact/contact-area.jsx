424c14c238b446e1082374aed956f3a3aa734cff
anastasiyaromana <158139413+anastasiyaromana@users.noreply.github.com>
2024-02-05 18:41:18 +0300

    Update contact-area.jsx

--- src/components/contact/contact-area.jsx
+++ src/components/contact/contact-area.jsx
@@ -12,10 +12,10 @@ const ContactArea = () => {
                     <div className="col-lg-6">
                         <div className="tp-contact-wrapper">
                             <div className="tp-contact-title-wrapper">
-                            <h3 className="tp-contact-title">Contact <span className="title-color">with us</span></h3> 
-                            <p>Email, call, or complete the form to learn how wAIw can <br /> 
-                             solve your subsurface challenges or book a demo <br />
-                             with our AI-specialists and business manager.</p>
+                            <h3 className="tp-contact-title">Get <span className="title-color">in touch</span></h3> 
+                            <p>If you have been in the field as long as we are, then you know - it gets lonely in here. <br /> 
+                                So, if you have in mind a proposal for collaboration or simply would like to let us know what you think, simply drop us an email.
+                                Or complete the form to learn how wAIw can solve your subsurface challenges and book a demo with our AI-specialists and business manager.</p>
                             </div>
                             <div className="tp-contact-content">
                             <div className="tp-contact-content-mail d-flex align-items-center">
