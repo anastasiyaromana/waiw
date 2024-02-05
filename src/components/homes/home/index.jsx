de83660d291b6c3263fa24cffe907d758719b7d5
anastasiyaromana <158139413+anastasiyaromana@users.noreply.github.com>
2024-02-05 16:56:44 +0300

    Update index.jsx

--- src/components/homes/home/index.jsx
+++ src/components/homes/home/index.jsx
@@ -22,8 +22,8 @@ const HomeOne = () => {
         <SupportArea />
         <FaqArea />
         <BlogArea />
-        <Footer />
         <ContactArea />
+        <Footer />
       </main>
     </>
   );
