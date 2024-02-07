c712e899f0318e6d446837c0cd464ec60a0214f3
anastasiyaromana <158139413+anastasiyaromana@users.noreply.github.com>
2024-02-08 02:27:15 +0300

    Update portfolio-data.js

--- src/data/portfolio-data.js
+++ src/data/portfolio-data.js
@@ -11,38 +11,20 @@ const portfolio_data = [
     {
         id: 1, 
         img: portfolio_img_1,
-        catagory: "Marketing",
-        title: "Business Growth",
+        catagory: "",
+        title: "Seismic processing: outperform conventional software with AI solutions",
     },
     {
         id: 2, 
         img: portfolio_img_2,
-        catagory: "XD Design",
-        title: "SEO & Content Writing",
+        catagory: "",
+        title: "Seismic interpretation: express structural model building with ML",
     },
     {
         id: 3, 
         img: portfolio_img_3,
-        catagory: "Business",
-        title: "Web Development",
-    },
-    {
-        id: 4, 
-        img: portfolio_img_4,
-        catagory: "Figma Design",
-        title: "Market Research",
-    },
-    {
-        id: 5, 
-        img: portfolio_img_5,
-        catagory: "Technology",
-        title: "IT Specialist",
-    },
-    {
-        id: 6, 
-        img: portfolio_img_6,
-        catagory: "Business Consulting",
-        title: "Business Analytics",
+        catagory: "",
+        title: "Generative AI: a new way to interact with projects using LLMs",
     },
 ]
 export default portfolio_data
