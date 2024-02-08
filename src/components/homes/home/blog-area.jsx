1dde52052ca6d80865b642edb9c40fe2353fd560
anastasiyaromana <158139413+anastasiyaromana@users.noreply.github.com>
2024-02-08 23:48:37 +0300

    Update blog-area.jsx

--- src/components/homes/home/blog-area.jsx
+++ src/components/homes/home/blog-area.jsx
@@ -29,8 +29,8 @@ const BlogArea = () => {
                             </div>
                             <div className="col-lg-6">
                             <div className="tp-blog-text justify-content-start justify-content-lg-end d-flex">
-                                <p>We actively share our expertise and insights through our Medium blog, " 
-                                    <br /> including guides like "Cook Your Own AI for Subsurface.
+                                <p>We actively share our expertise and insights through our Medium blog, 
+                                    <br /> including guides like "Cook Your Own AI for Subsurface".
                                 </p>
                             </div>
                             </div>
