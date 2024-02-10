6e7a0689ae2b6fd3e0100386a23a94b590dc5dcd
anastasiyaromana <158139413+anastasiyaromana@users.noreply.github.com>
2024-02-10 18:21:33 +0300

    Update index.jsx

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -30,7 +30,7 @@ const About = () => {
             <BlogArea />
             <FooterContact />
           </main>
-          <FooterThree />
+         {/* <FooterThree /> */}
         </>
     );
 };
