011547d0cbbc12fe9c171310986a4dcc208158e6
anastasiyaromana <158139413+anastasiyaromana@users.noreply.github.com>
2024-02-10 14:24:40 +0300

    Update video-popup.jsx

--- src/modals/video-popup.jsx
+++ src/modals/video-popup.jsx
@@ -4,7 +4,7 @@ import ModalVideo from "react-modal-video";
 const VideoPopup = ({
   isVideoOpen,
   setIsVideoOpen,
-  videoId = "bgMEvrd2E",
+  videoId = "O9nV-YsO1wU",
 }) => {
   return (
     <>
