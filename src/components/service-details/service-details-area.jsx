e4ac79358a8e434fd7fee875e46e87b6ac2742b0
anton-br <ziliboba753@gmail.com>
2024-02-11 11:43:34 +0300

    fix newlines

--- src/components/service-details/service-details-area.jsx
+++ src/components/service-details/service-details-area.jsx
@@ -32,15 +32,15 @@ const service_details_content = {
          "Innovation and Speed of Development",
          "Quality and Reliability",
          ],
-      "description_3": "1. Transparency and Trust: Open-source software's code is accessible for review, modification, and distribution. This transparency allows users to understand how the software operates, potentially increasing trust, especially in sensitive applications like AI where understanding the decision-making process or data handling is crucial.\n"+
-           "2. Security: While open-source software is exposed to everyone, this can be a strength. More eyes on the code mean that security vulnerabilities can be identified and fixed rapidly by the community, as opposed to closed-source software, where vulnerabilities might go unnoticed or unaddressed until the company decides to act.\n"+
-           "3. Customization and Flexibility: Open-source AI software can be freely modified to meet specific user needs. This adaptability can be particularly beneficial for businesses with unique requirements that off-the-shelf closed-source solutions cannot meet without expensive custom development.\n"+
-           "4. Cost-Effectiveness: Open-source software is generally free to use, modify, and distribute. This can significantly reduce costs for businesses, especially startups or those looking to scale up, as there are no licensing fees or costs associated with product changes.\n"+
-           "5. Community Support and Collaboration: Open-source projects often have active communities. Users can benefit from the collective knowledge and experience of a vast community of developers and users. This can lead to better support, more robust features, and faster resolution of issues compared to relying solely on a single vendor's support team.\n"+
-           "6. Avoidance of Vendor Lock-in: With closed-source software, customers are often tied to one vendor's ecosystem, subject to their pricing, updates, and the future direction of the software. Open-source software offers an escape from vendor lock-in, providing the freedom to switch service providers or customize the software without contractual restrictions or the risk of obsolescence.\n"+
-           "7. Innovation and Speed of Development: Open-source projects can evolve more quickly than their closed-source counterparts due to contributions from a global community. Diverse perspectives lead to innovative features and solutions, driving the rapid advancement of technology in ways that a single company might not achieve on its own.\n"+
-           "8. Quality and Reliability: A common misconception is that open-source software is less reliable. However, with many users and contributors, issues are quickly identified and resolved. The collaborative nature of open-source development often leads to high-quality, well-tested, and reliable software solutions.`,\n"
-
+      "description_3": ["1. Transparency and Trust: Open-source software's code is accessible for review, modification, and distribution. This transparency allows users to understand how the software operates, potentially increasing trust, especially in sensitive applications like AI where understanding the decision-making process or data handling is crucial.",
+           "2. Security: While open-source software is exposed to everyone, this can be a strength. More eyes on the code mean that security vulnerabilities can be identified and fixed rapidly by the community, as opposed to closed-source software, where vulnerabilities might go unnoticed or unaddressed until the company decides to act.",
+           "3. Customization and Flexibility: Open-source AI software can be freely modified to meet specific user needs. This adaptability can be particularly beneficial for businesses with unique requirements that off-the-shelf closed-source solutions cannot meet without expensive custom development.",
+           "4. Cost-Effectiveness: Open-source software is generally free to use, modify, and distribute. This can significantly reduce costs for businesses, especially startups or those looking to scale up, as there are no licensing fees or costs associated with product changes.",
+           "5. Community Support and Collaboration: Open-source projects often have active communities. Users can benefit from the collective knowledge and experience of a vast community of developers and users. This can lead to better support, more robust features, and faster resolution of issues compared to relying solely on a single vendor's support team.",
+           "6. Avoidance of Vendor Lock-in: With closed-source software, customers are often tied to one vendor's ecosystem, subject to their pricing, updates, and the future direction of the software. Open-source software offers an escape from vendor lock-in, providing the freedom to switch service providers or customize the software without contractual restrictions or the risk of obsolescence.",
+           "7. Innovation and Speed of Development: Open-source projects can evolve more quickly than their closed-source counterparts due to contributions from a global community. Diverse perspectives lead to innovative features and solutions, driving the rapid advancement of technology in ways that a single company might not achieve on its own.",
+           "8. Quality and Reliability: A common misconception is that open-source software is less reliable. However, with many users and contributors, issues are quickly identified and resolved. The collaborative nature of open-source development often leads to high-quality, well-tested, and reliable software solutions.`"
+      ]
       }, 
       {id: 2, title: "second",
       description: "second",
@@ -56,14 +56,14 @@ const service_details_content = {
          "Second",
          "Second",
          ],
-      "description_3": "1. second.\n"+
-           "2. second.\n"+
-           "3. second.\n"+
-           "4. second.\n"+
-           "5. second.\n"+
-           "6. second.\n"+
-           "7. second.\n"+
-           "8. second,\n"
+      "description_3": ["1. second.",
+           "2. second.",
+           "3. second.",
+           "4. second.",
+           "5. second.",
+           "6. second.",
+           "7. second.",
+           "8. second,"]
       }],
 }
 const {service_details_tab, details_body_tab} = service_details_content
@@ -141,7 +141,11 @@ const ServiceDetailsArea = () => {
                               </div>
                            </div>
                               </div>
-                           <p> { details_body_tab[NumPage].description_3 } </p>
+                              <ul>
+                                 {details_body_tab[NumPage].description_3.map((item, i) => 
+                                 <p>{item}</p> 
+                                 )} 
+                              </ul>
                         </div>
                      </div>
                   <div className="tp-service-details-faq faq-style-1">
